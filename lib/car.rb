2314bfb2c2ba167f19c2e1f2dea56ebda1cf1ed1
ellevalentine <elle.valentine@hotmail.com>
2019-04-09 08:57:18 +0100

    done

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,14 @@
-require_relative "./vehicle.rb"
+require_relative "./vehicle.rb" # Our Car class will need access to the Vehicle class and will therefore need access to the file that contains that class.
+
+class Car < Vehicle  # How to define car class as a subset of Vehicle - this class car will be known as the child class/ subclass to vehicle / since a car is a type of vehicle and will have similar traits to other vehicle
+
+# this class has inherited all methods from parents class so we alrady pass:
+# initializes with two arguments, wheel size and number
+#    has a wheel_size /   has a wheel_number /   returns 'filling up!' - as required in our tests
+
+# the only test failing is the :   #go - returns 'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!!' which is different from our go method in our parent class so we have over write this by writing a new method go
+def go # writing a new method for go in child class to override the go method in parent class
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+
+end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,21 @@
-class Vehicle
+class Vehicle # The class vehicle will be out parent/super class for this model - since there is multiple type of vehicles you can have as subclasses
+  attr_accessor :wheel_size, :wheel_number # gives us access to both methods
 
+#most vehicles will have a wheel size and amount of wheels even when they are different types of vehicles - so these are commmon features
+
+
+    def initialize(wheel_size, wheel_number) #initializes with two arguments, wheel size and number
+      @wheel_size = wheel_size # set to the passing size
+      @wheel_number = wheel_number #set to the passing number of wheels
+    end
+
+ # both below methods are ones in which we could call upon different types of vehicles / describe some common vehicle behavior.
+
+    def go # method to return below string
+      "vrrrrrrrooom!"
+    end
+
+    def fill_up_tank #method to return below string
+      "filling up!"
+    end
 end
